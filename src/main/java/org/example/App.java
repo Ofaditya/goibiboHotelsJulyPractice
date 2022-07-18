df26ffe185ca2928bd602b244ce0fdb4aac8fe6d
Aditya S <adityasalunkhe4@gmail.com>
2022-07-18 12:10:24 +0530

    Added actions/methods in HotelsPage.class

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -25,44 +25,11 @@ public class App extends BaseClass {
         hotelpage = new HotelsPage();
         driver.navigate().to("https://www.goibibo.com/hotels");
 
-        /* 1.Keys class method -*/
-        hotelpage.RadioButtonInt.click();
-        hotelpage.AreaName.click();
-        hotelpage.AreaName.sendKeys("Mumbai");
-        Thread.sleep(2000);
-        hotelpage.AreaName.sendKeys(Keys.ARROW_DOWN, Keys.ENTER);
-        /* 2. Action class method -
-        Actions act = new Actions(driver);
-        WebElement loc = driver.findElement(By.xpath("//p[contains (text(),'Amsterdam')]"));
-        act.moveToElement(loc).click().build().perform();*/
-
-        hotelpage.openCheckinCalendar.click();
-        setdate(hotelpage.dateStart, "19");
-        System.out.println("check in date set");
-        setdate(hotelpage.dateStart, "29");
-        System.out.println("check out date set");
-
-        hotelpage.GuestsAndRooms.click();
-        hotelpage.RoomsDecrement.click();
-        Thread.sleep(2000);
-        String alertMSG1 = driver.switchTo().alert().getText();
-        System.out.println("Alert msg for min Rooms count :-" + alertMSG1);
-        driver.switchTo().alert().accept();
-
-        hotelpage.AdultsDecrement.click();
-        hotelpage.AdultsDecrement.click();
-        Thread.sleep(2000);
-        String alertMSG2 = driver.switchTo().alert().getText();
-        System.out.println("Alert msg for min Adults count :-" + alertMSG2);
-        driver.switchTo().alert().accept();
-
-        hotelpage.ChildDecrement.click();
-        Thread.sleep(2000);
-        String alertMSG3 = driver.switchTo().alert().getText();
-        System.out.println("Alert msg for min Children count :-" + alertMSG3);
-        driver.switchTo().alert().accept();
-
-        hotelpage.DoneBtn.click();
-        hotelpage.SearchBtn.click();
+        hotelpage.ClkRadioBtn();
+        hotelpage.EnterArea();
+        hotelpage.SelCalenderDate();
+        hotelpage.SelGuestRoomChildCount();
+        hotelpage.ClkDoneBtn();
+        hotelpage.ClkSearchBtn();
     }
 }
--- src/main/java/org/example/HotelsPage.java
+++ src/main/java/org/example/HotelsPage.java
@@ -1,7 +1,9 @@
 package org.example;
 
 import Util.BaseClass;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.FindBys;
 import org.openqa.selenium.support.PageFactory;
@@ -11,7 +13,6 @@ import java.util.List;
 
 public class HotelsPage extends BaseClass {
     public HotelsPage(){
-        this.driver= driver;
         PageFactory.initElements(driver,this);
     }
 
@@ -20,6 +21,9 @@ public class HotelsPage extends BaseClass {
 
     @FindBy(xpath = "//input[@placeholder='e.g. - Area, Landmark or Property Name']")
     WebElement AreaName;
+
+    @FindBy(xpath = "//p[contains (text(),'Amsterdam')]")
+    WebElement AreafrmDropdown;
 
     @FindBy(xpath = "//div[@data-testid='openCheckinCalendar']")
     WebElement openCheckinCalendar;
@@ -45,5 +49,60 @@ public class HotelsPage extends BaseClass {
     @FindBy(xpath = "//button[contains (text(), 'Search')]")
     WebElement SearchBtn;
 
+    public void ClkRadioBtn(){
+        RadioButtonInt.click();
+    }
+
+    public void EnterArea() throws InterruptedException {
+        AreaName.click();
+
+        /* 1.Keys class method -*/
+        AreaName.sendKeys("Mumbai");
+        Thread.sleep(2000);
+        AreaName.sendKeys(Keys.ARROW_DOWN, Keys.ENTER);
+
+        /* 2. Action class method -
+        Actions act = new Actions(driver);
+        act.moveToElement(AreafrmDropdown).click().build().perform();*/
+    }
+
+    public void SelCalenderDate(){
+        openCheckinCalendar.click();
+        setdate(dateStart, "19");
+        System.out.println("check in date set");
+        setdate(dateStart, "29");
+        System.out.println("check out date set");
+    }
+
+    public void SelGuestRoomChildCount() throws InterruptedException {
+        GuestsAndRooms.click();
+        RoomsDecrement.click();
+        Thread.sleep(1000);
+        String alertMSG1 = driver.switchTo().alert().getText();
+        System.out.println("Alert msg for min Rooms count :-" + alertMSG1);
+        driver.switchTo().alert().accept();
+
+        AdultsDecrement.click();
+        AdultsDecrement.click();
+        Thread.sleep(1000);
+        String alertMSG2 = driver.switchTo().alert().getText();
+        System.out.println("Alert msg for min Adults count :-" + alertMSG2);
+        driver.switchTo().alert().accept();
+
+        ChildDecrement.click();
+        Thread.sleep(1000);
+        String alertMSG3 = driver.switchTo().alert().getText();
+        System.out.println("Alert msg for min Children count :-" + alertMSG3);
+        driver.switchTo().alert().accept();
+    }
+
+    public void ClkDoneBtn(){
+        DoneBtn.click();
+    }
+
+    public void ClkSearchBtn(){
+        SearchBtn.click();
+        System.out.println("Search Comoplated -----------------------------");
+    }
 
 }
