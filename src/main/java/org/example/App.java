8a3a8e5fefc5a14ef1a8da39eae6029a2e6b8167
Aditya S <adityasalunkhe4@gmail.com>
2022-07-18 11:32:42 +0530

    Added POM structure - Hotelpage class and BaseClass

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import Util.BaseClass;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -12,65 +13,56 @@ import java.time.Duration;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-public class App 
-{
-    public static void setdate(WebDriver driver, String date){
-        List<WebElement> dateStart = driver.findElements(By.xpath("//li[@class='date_is_selectable_true']/span"));
-        for (int i=0;i<dateStart.size();i++)
-        {
-            String d = dateStart.get(i).getText();
-            if (d.equalsIgnoreCase(date))
-            {
-                dateStart.get(i).click();
-                break;
-            }
-        }
-    }
-    public static void main( String[] args ) throws InterruptedException {
-        WebDriver driver = WebDriverManager.chromedriver().avoidShutdownHook().create();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+public class App extends BaseClass {
+
+    static HotelsPage hotelpage;
+
+
+    public static void main(String[] args) throws InterruptedException {
+
+        BaseClass.GetBrowser();
+
+        hotelpage = new HotelsPage();
         driver.navigate().to("https://www.goibibo.com/hotels");
 
-       /* 1.Keys class method -*/
-        driver.findElement(By.xpath("//h4[contains (text(),'International')]")).click();
-        driver.findElement(By.xpath("//input[@placeholder='e.g. - Area, Landmark or Property Name']")).click();
-        driver.findElement(By.xpath("//input[@placeholder='e.g. - Area, Landmark or Property Name']")).sendKeys("Mumbai");
+        /* 1.Keys class method -*/
+        hotelpage.RadioButtonInt.click();
+        hotelpage.AreaName.click();
+        hotelpage.AreaName.sendKeys("Mumbai");
         Thread.sleep(2000);
-        driver.findElement(By.xpath("//input[@placeholder='e.g. - Area, Landmark or Property Name']")).sendKeys(Keys.ARROW_DOWN,Keys.ENTER);
+        hotelpage.AreaName.sendKeys(Keys.ARROW_DOWN, Keys.ENTER);
         /* 2. Action class method -
         Actions act = new Actions(driver);
         WebElement loc = driver.findElement(By.xpath("//p[contains (text(),'Amsterdam')]"));
         act.moveToElement(loc).click().build().perform();*/
 
-        driver.findElement(By.xpath("//div[@data-testid='openCheckinCalendar']")).click();
-        setdate(driver, "19");
+        hotelpage.openCheckinCalendar.click();
+        setdate(hotelpage.dateStart, "19");
         System.out.println("check in date set");
-        setdate(driver, "29");
+        setdate(hotelpage.dateStart, "29");
         System.out.println("check out date set");
 
-        driver.findElement(By.xpath("//span[contains (text(), 'Guests & Rooms')]")).click();
-        driver.findElement(By.xpath("//span[contains (text(), 'Rooms')]/following-sibling::div/span")).click();
+        hotelpage.GuestsAndRooms.click();
+        hotelpage.RoomsDecrement.click();
         Thread.sleep(2000);
-        String alertMSG1 =driver.switchTo().alert().getText();
-        System.out.println("Alert msg for min Rooms count :-"+alertMSG1);
+        String alertMSG1 = driver.switchTo().alert().getText();
+        System.out.println("Alert msg for min Rooms count :-" + alertMSG1);
         driver.switchTo().alert().accept();
 
-        driver.findElement(By.xpath("//span[contains (text(), 'Adults')]/following-sibling::div/span")).click();
-        driver.findElement(By.xpath("//span[contains (text(), 'Adults')]/following-sibling::div/span")).click();
+        hotelpage.AdultsDecrement.click();
+        hotelpage.AdultsDecrement.click();
         Thread.sleep(2000);
-        String alertMSG2 =driver.switchTo().alert().getText();
-        System.out.println("Alert msg for min Adults count :-"+alertMSG2);
+        String alertMSG2 = driver.switchTo().alert().getText();
+        System.out.println("Alert msg for min Adults count :-" + alertMSG2);
         driver.switchTo().alert().accept();
 
-
-        driver.findElement(By.xpath("//span[contains (text(), 'Children')]/following-sibling::div/span")).click();
+        hotelpage.ChildDecrement.click();
         Thread.sleep(2000);
-        String alertMSG3 =driver.switchTo().alert().getText();
-        System.out.println("Alert msg for min Children count :-"+alertMSG3);
+        String alertMSG3 = driver.switchTo().alert().getText();
+        System.out.println("Alert msg for min Children count :-" + alertMSG3);
         driver.switchTo().alert().accept();
 
-        driver.findElement(By.xpath("//button[contains (text(), 'Done')]")).click();
-        driver.findElement(By.xpath("//button[contains (text(), 'Search')]")).click();
+        hotelpage.DoneBtn.click();
+        hotelpage.SearchBtn.click();
     }
 }
